1cc0b4ea1aa5975d4cbc8fda93dbbc2627bf733b
Tianxiang Li <tl3024@columbia.edu>
2024-10-02 12:41:27 +0800

    make it function

--- record_battery.sh
+++ record_battery.sh
@@ -24,26 +24,37 @@ elif [ ! -s "$csv_file" ]; then
     write_header
 fi
 
-# Fetch and format battery information
-battery_info=$(pmset -g batt)
-# If there are two lines, use the second line
-if [ $(echo "$battery_info" | wc -l) -eq 2 ]; then
-    battery_info=$(echo "$battery_info" | sed -n '2p')
-fi
+# Function to get battery percentage and charging status
+get_battery_info() {
+    local battery_info=$(pmset -g batt)
+    # If there are two lines, use the second line
+    if [ $(echo "$battery_info" | wc -l) -eq 2 ]; then
+        battery_info=$(echo "$battery_info" | sed -n '2p')
+    fi
 
-percentage=$(echo "$battery_info" | awk -F '; ' 'NR==1 {print $1}' | awk '{print $NF}' | awk -F '%' '{print $1}')
-battery_status=$(echo "$battery_info" | awk -F '; ' 'NR==1 {print $2}')
-if [ "$battery_status" = "discharging" ]; then
-    charging=0
-elif [ "$battery_status" = "charging" ]; then
-    charging=1
-elif [ "$battery_status" = "AC attached" ]; then
-    charging=2
-elif [ "$battery_status" = "charged" ]; then
-    charging=2
-else
-    charging=3
-fi
+    local percentage=$(echo "$battery_info" | awk -F '; ' 'NR==1 {print $1}' | awk '{print $NF}' | awk -F '%' '{print $1}')
+    local battery_status=$(echo "$battery_info" | awk -F '; ' 'NR==1 {print $2}')
+    local charging
+    if [ "$battery_status" = "discharging" ]; then
+        charging=0
+    elif [ "$battery_status" = "charging" ]; then
+        charging=1
+    elif [ "$battery_status" = "AC attached" ]; then
+        charging=2
+    elif [ "$battery_status" = "charged" ]; then
+        charging=2
+    else
+        charging=3
+    fi
+
+    echo "${percentage},${charging}"
+}
+
+# Get battery information
+battery_info=$(get_battery_info)
+percentage=$(echo "$battery_info" | cut -d',' -f1)
+charging=$(echo "$battery_info" | cut -d',' -f2)
+
 current_time=$(date "+%Y-%m-%d %H:%M:%S %z")
 # Append to CSV file
 echo "${current_time},${percentage},${charging}" >> $csv_file
